2ef1e082100b7ee6fbfe208a77dd975622b9664b
Al Snow <jasnow@hotmail.com>
2023-03-06 17:35:43 -0500

    Upgraded [docile, slim, tilt] gems; Added simplecov gem

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,12 @@
+require 'simplecov'
+if ENV["COVERAGE"]
+  SimpleCov.start 'rails' do
+    puts "Adding simplecov"
+    enable_coverage :branch
+    primary_coverage :branch
+  end
+end
+
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
